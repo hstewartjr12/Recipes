c465d2229b21f55e8ad7d56f4dc2d373cc998e2d
Henry <hstew21@icloud.com>
2025-05-16 22:22:20 -0400

    Initial Commit

---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,16 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Henry Stewart on 5/16/25.
+//
+
+import Testing
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
